b1e3d39864739c446754379cd58d1b049b62f38e
Daniel <daniel@developerdan.com>
2019-12-12 09:20:39 -0500

    Improve auto-tagging logic to get the largest version number tag

--- github-commit-auto-updates.sh
+++ github-commit-auto-updates.sh
@@ -14,7 +14,7 @@ git config --global user.email "daniel@developerdan.com"
 git config --global user.name "Auto Updates"
 git add ./docs/rules-v1.json
 git commit -m "Automatic github actions updates"
-LAST_TAG=$(git describe --tags | sed 's/\([\d\.]*\)-.*/\1/')
+LAST_TAG=$(git tag -l --sort=v:refname | tail -1)
 echo "Last tag: ${LAST_TAG}"
 NEW_TAG="${LAST_TAG%.*}.$((${LAST_TAG##*.}+1))"
 echo "Last tag: ${NEW_TAG}"
