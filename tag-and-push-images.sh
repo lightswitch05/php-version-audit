ab32fd2f00cf03959a2b1ca2ad52e7636b464314
Daniel <daniel@developerdan.com>
2021-06-09 13:47:36 +0000

    Enable debugging for tag-and-push-images.sh - bump up cron to speed up testing

--- tag-and-push-images.sh
+++ tag-and-push-images.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 # Usage: ./tag-and-push-images.sh ${docker-compose-service-name}
 # examples:
@@ -12,16 +12,22 @@ function main() {
     IMAGE="lightswitch05/php-version-audit"
 
     # Build and tag primary tag name
+    echo "Building ${target}"
     docker-compose build --pull "${target}"
+    echo "Pushing ${IMAGE}:${target}"
     docker push "${IMAGE}:${target}"
 
     # Version-based tag name
+    echo "Tagging ${IMAGE}:${target} as ${IMAGE}:${MAJOR_VERSION}-${target}"
     docker tag "${IMAGE}:${target}" "${IMAGE}:${MAJOR_VERSION}-${target}"
-    docker push "${IMAGE}:${target}"
+    echo "Pushing ${IMAGE}:${MAJOR_VERSION}-${target}"
+    docker push "${IMAGE}:${MAJOR_VERSION}-${target}"
 
     # Latest tag name
     if [[ "${target}" = "${DEFAULT_TAG}" ]]; then
+        echo "Tagging ${IMAGE}:${target}" "${IMAGE}:latest"
         docker tag "${IMAGE}:${target}" "${IMAGE}:latest"
+        echo "Pushing ${IMAGE}:latest"
         docker push "${IMAGE}:latest"
     fi
 }
