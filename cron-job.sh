4d0c5dfeb5817959999e6b06d82aa6cdf9a13905
Daniel <daniel@developerdan.com>
2022-04-06 13:13:08 -0400

    Reset patch version daily

--- cron-job.sh
+++ cron-job.sh
@@ -23,16 +23,29 @@ fi
 
 git add ./docs/rules-v1.json
 git commit -m "${COMMIT_MESSAGE}"
+
 LAST_TAG=$(git tag -l --sort=v:refname | tail -1)
 echo "Last tag: ${LAST_TAG}"
 MAJOR_VERSION="${LAST_TAG%%.*}"
 echo "Major version: ${MAJOR_VERSION}"
+
+OLD_MINOR_VERSION="${LAST_TAG%.*}"
+OLD_MINOR_VERSION="${OLD_MINOR_VERSION##*.}"
+echo "Old Minor version: ${OLD_MINOR_VERSION}"
 MINOR_VERSION=$(date +"%Y%m%d")
-echo "Minor version: ${MINOR_VERSION}"
-PATCH_VERSION="${LAST_TAG##*.}"
+echo "New Minor version: ${MINOR_VERSION}"
+
+if [[ "${OLD_MINOR_VERSION}" == "${MINOR_VERSION}" ]]; then
+    PATCH_VERSION="${LAST_TAG##*.}"
+    PATCH_VERSION="$((PATCH_VERSION+1))"
+else
+    PATCH_VERSION="0"
+fi
 echo "Patch version: ${PATCH_VERSION}"
-NEW_TAG="${MAJOR_VERSION}.${MINOR_VERSION}.$((PATCH_VERSION+1))"
+
+NEW_TAG="${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}"
 echo "New tag: ${NEW_TAG}"
+
 git tag "${NEW_TAG}"
 git push
 git push gitlab
