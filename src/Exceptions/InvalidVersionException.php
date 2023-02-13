8d95f79049da4c7d547831191d9030029db107a9
Daniel <daniel@developerdan.com>
2023-02-12 20:06:37 -0500

    Revert "Revert "Feature: PHP 8.0""

--- src/Exceptions/InvalidVersionException.php
+++ src/Exceptions/InvalidVersionException.php
@@ -1,15 +1,11 @@
 <?php
+
 declare(strict_types=1);
 
 namespace lightswitch05\PhpVersionAudit\Exceptions;
 
-
 final class InvalidVersionException extends \InvalidArgumentException
 {
-    /**
-     * @param string|null $version
-     * @return InvalidVersionException
-     */
     public static function fromString(?string $version): InvalidVersionException
     {
         return new self("PhpVersion [$version] is not valid");
--- src/Exceptions/ParseException.php
+++ src/Exceptions/ParseException.php
@@ -1,26 +1,16 @@
 <?php
+
 declare(strict_types=1);
 
 namespace lightswitch05\PhpVersionAudit\Exceptions;
 
-
 final class ParseException extends \ErrorException
 {
-    /**
-     * @param string|null $message
-     * @return ParseException
-     */
     public static function fromString(?string $message): ParseException
     {
         return new self("Parse error: $message");
     }
 
-    /**
-     * @param \Exception $ex
-     * @param string     $fileName
-     * @param int        $line
-     * @return ParseException
-     */
     public static function fromException(\Exception $ex, string $fileName, int $line): ParseException
     {
         return new self($ex->getMessage(), $ex->getCode(), 1, $fileName, $line, $ex);
